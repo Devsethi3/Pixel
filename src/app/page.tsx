6339792fb063107b9a994a2edae4e6d5626faaaa
Devsethi3 <devsethi4522@gmail.com>
2026-03-28 21:36:44 +0530

    Update page.tsx and remove terminal process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
         <Suspense fallback={<GridSkeleton />}>
           <ShaderGrid />
         </Suspense>
-        <TerminalProcess />
+        {/* <TerminalProcess /> */}
       </section>
       <Footer />
     </>
